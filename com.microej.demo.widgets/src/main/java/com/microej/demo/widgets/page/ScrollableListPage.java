84043f64d6b200f9e592a7c943c11ebdf4f2de90
Sébastien Eon <seon@is2t.com>
2015-12-02 11:22:45 +0100

    WI14244 Load complete list after a delay.

--- com.microej.demo.widgets/src/main/java/com/microej/demo/widgets/page/ScrollableListPage.java
+++ com.microej.demo.widgets/src/main/java/com/microej/demo/widgets/page/ScrollableListPage.java
@@ -8,8 +8,12 @@ package com.microej.demo.widgets.page;
 
 import com.microej.demo.widgets.style.ClassSelectors;
 
+import ej.bon.Timer;
+import ej.bon.TimerTask;
+import ej.components.dependencyinjection.ServiceLoaderFactory;
 import ej.composite.ListComposite;
 import ej.composite.ScrollComposite;
+import ej.microui.display.Display;
 import ej.mwt.Widget;
 import ej.widget.basic.Label;
 
@@ -18,7 +22,11 @@ import ej.widget.basic.Label;
  */
 public class ScrollableListPage extends AbstractDemoPage {
 
+	private static final int APPEARANCE_DELAY = 1000;
 	private static final int ITEM_COUNT = 100;
+	private static final int FIRST_SHOT_COUNT = 20;
+	private ListComposite listComposite;
+	private boolean complete;
 
 	@Override
 	protected String getTitle() {
@@ -37,16 +45,42 @@ public class ScrollableListPage extends AbstractDemoPage {
 		// Item n-1
 		// Item n
 
-		ListComposite listComposite = new ListComposite();
-		listComposite.setHorizontal(false);
+		this.listComposite = new ListComposite();
+		this.listComposite.setHorizontal(false);
 
-		for (int i = 1; i <= ITEM_COUNT; i++) {
+		for (int i = 1; i <= FIRST_SHOT_COUNT; i++) {
 			Label item = new Label("Item " + i); //$NON-NLS-1$
-			listComposite.add(item);
 			item.addClassSelector(ClassSelectors.LIST_ITEM);
+			this.listComposite.add(item);
 		}
 
-		return new ScrollComposite(listComposite, true);
+		return new ScrollComposite(this.listComposite, true);
+	}
+
+	@Override
+	public void showNotify() {
+		super.showNotify();
+		// Add missing items.
+		Timer timer = ServiceLoaderFactory.getServiceLoader().getService(Timer.class);
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				Display.getDefaultDisplay().callSerially(new Runnable() {
+					@Override
+					public void run() {
+						if (!ScrollableListPage.this.complete) {
+							ScrollableListPage.this.complete = true;
+							for (int i = FIRST_SHOT_COUNT + 1; i <= ITEM_COUNT; i++) {
+								Label item = new Label("Item " + i); //$NON-NLS-1$
+								item.addClassSelector(ClassSelectors.LIST_ITEM);
+								ScrollableListPage.this.listComposite.add(item);
+							}
+						}
+					}
+				});
+				revalidate();
+			}
+		}, APPEARANCE_DELAY);
 	}
 
 }

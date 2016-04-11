9a94a2b6bacad12f51f7707dc69dfaa7510d6045
privron <privron@is2t-intern.com>
2016-04-11 17:11:48 +0200

    Move to ej.demo.ui.widget use new .list files

---
+++ ej.demo.ui.widget/src/main/java/ej/demo/ui/widget/page/ImageWidgetPage.java
@@ -0,0 +1,47 @@
+/*
+ * Java
+ *
+ * Copyright 2014-2015 IS2T. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be found at http://www.is2t.com/open-source-bsd-license/.
+ */
+package ej.demo.ui.widget.page;
+
+import ej.widget.basic.AbstractSlider;
+import ej.widget.basic.image.ImageCheck;
+import ej.widget.basic.image.ImageRadio;
+import ej.widget.basic.image.ImageSlider;
+import ej.widget.basic.image.ImageSwitch;
+import ej.widget.composed.Toggle;
+import ej.widget.composed.ToggleComposite;
+import ej.widget.toggle.RadioModel;
+
+/**
+ * This page illustrates the widgets rendered with some images.
+ */
+public class ImageWidgetPage extends WidgetPage {
+
+	@Override
+	protected String getTitle() {
+		return "Images"; //$NON-NLS-1$
+	}
+
+	@Override
+	protected ToggleComposite newCheckBox(String string) {
+		return new Toggle(new ImageCheck(), string);
+	}
+
+	@Override
+	protected ToggleComposite newRadioButton(String string) {
+		return new Toggle(new RadioModel(), new ImageRadio(), string);
+	}
+
+	@Override
+	protected ToggleComposite newSwitch(String string) {
+		return new Toggle(new ImageSwitch(), string);
+	}
+
+	@Override
+	protected AbstractSlider newSlider(int min, int max, int initial) {
+		return new ImageSlider(min, max, initial);
+	}
+}
---
+++ ej.demo.ui.widget/src/main/java/ej/demo/ui/widget/page/ScrollableTextPage.java
@@ -0,0 +1,72 @@
+/*
+ * Java
+ *
+ * Copyright 2015 IS2T. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be found at http://www.is2t.com/open-source-bsd-license/.
+ */
+package ej.demo.ui.widget.page;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import ej.container.List;
+import ej.container.Scroll;
+import ej.demo.ui.widget.style.ClassSelectors;
+import ej.mwt.Widget;
+import ej.widget.basic.Label;
+
+/**
+ * This page illustrates the scrollable text.
+ */
+public class ScrollableTextPage extends AbstractDemoPage {
+
+	@Override
+	protected String getTitle() {
+		return "Scrollable text"; //$NON-NLS-1$
+	}
+
+	@Override
+	protected Widget createMainContent() {
+		List listComposite = new List(false);
+
+		add("MicroEJ SDK", "sdk.txt", listComposite); //$NON-NLS-1$ //$NON-NLS-2$
+		add("MicroEJ Studio", "studio.txt", listComposite); //$NON-NLS-1$ //$NON-NLS-2$
+		add("MicroEJ Application Store", "store.txt", listComposite); //$NON-NLS-1$ //$NON-NLS-2$
+		add("MicroEJ OS", "os.txt", listComposite); //$NON-NLS-1$ //$NON-NLS-2$
+
+		return new Scroll(false, listComposite, true);
+	}
+
+	private void add(String title, String filename, List listComposite) {
+		Label titleLabel = new Label();
+		titleLabel.addClassSelector(ClassSelectors.TEXT_TITLE);
+		titleLabel.setText(title);
+
+		String description = read(filename);
+		Label descriptionLabel = new Label();
+		descriptionLabel.addClassSelector(ClassSelectors.MULTILINE);
+		descriptionLabel.setText(description);
+
+		Label emptyLabel = new Label();
+
+		listComposite.add(titleLabel);
+		listComposite.add(descriptionLabel);
+		listComposite.add(emptyLabel);
+	}
+
+	private String read(String filename) {
+		try {
+			InputStream inputStream = ScrollableTextPage.class.getResourceAsStream(filename);
+			StringBuilder textBuffer = new StringBuilder();
+			byte[] buffer = new byte[128];
+			while (inputStream.available() != 0) {
+				int read = inputStream.read(buffer);
+				textBuffer.append(new String(buffer, 0, read));
+			}
+			return textBuffer.toString();
+		} catch (IOException e) {
+			return ""; //$NON-NLS-1$
+		}
+	}
+
+}
